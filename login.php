f4f86b1a21cbd91fa5f0e551ce5efc5e51cdc847
ryanomongos <60454418+ryanomongos@users.noreply.github.com>
2020-10-13 22:03:14 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>        
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                  <form action="", method="POST">
+                      <h5>Username</h5>
+                      <input type="text" name="username" required><br />
+                      <h5>Password</h5>
+                      <input type="text" name="username" required><br /><br />
+                      <button name="submit">Submit</button><br /><br />
+                      <h7>If you haven't registered</h7><br />
+                      <a><button>Register</button></a>
+                  </form>
+                </div>
+              </div>
+        </div>
+    </body>
+</html>
