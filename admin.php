e22c51203a90d9cb85a479443c79ae9eee890ede
ryanomongos <60454418+ryanomongos@users.noreply.github.com>
2020-10-14 00:05:41 +0800

    With admin page

---
+++ admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>        
+        <div class="container">
+            <?php
+                $conn = new mysqli('localhost', 'root', '', 'exercise_database');
+
+                $result = $conn->query("SELECT * FROM user_table");
+
+                while($row = $result->fetch_assoc()){
+            ?>
+                <div class="card">
+                    <br />Name: <?php echo $row['name']?><br />
+                    Course: <?php echo $row['username']?><br />
+                    Email: <?php echo $row['password']?><br />
+                </div>
+            <?php
+                }
+            ?>
+            <h2>Customer Data</h2>
+            <table>
+                <tr>
+                    <th>NAME</th>
+                    <th>USERNAME</th>
+                    <th>PASSWORD</th>
+                <tr>
+                <?php
+                    $conn = new mysqli('localhost', 'root', '', 'activity1');
+
+                    $result = $conn->query("SELECT user_type FROM user_accounts WHERE user_type == 'client'");
+
+                    while($row = $result->fetch_assoc()){
+                        echo "<tr>
+                                <td>".$row['name']."</td>
+                                <td>".$row['username']."</td>
+                                <td>".$row['password']."</td>
+                            </tr>";
+                    }
+                ?>
+            </table>            
+        </div>
+    </body>
+</html>
